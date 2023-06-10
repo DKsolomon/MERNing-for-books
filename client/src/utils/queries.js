68454d7e80df6d0d0ac45e2ed7fe384932c0d786
DKsolomon <DKSolomon8135@gmail.com>
2023-06-09 17:08:32 -0700

    fixed connection.js, added queries and mutations file on client side

---
+++ client/src/utils/queries.js
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+
+export const GET_ME = gql`
+    query me {
+        me {
+            _id
+            email
+            username
+            bookCount
+            savedBooks {
+                authors
+                bookId
+                description
+                image
+                link
+                title
+            }
+            
+  }
+    }`
